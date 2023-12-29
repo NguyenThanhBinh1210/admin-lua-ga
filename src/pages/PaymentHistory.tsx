4ec4dd2f1d92e283ef8a23eccb27683e0edc5479
catalina5 <thanhbinh12369yt@gmail.com>
2023-12-29 21:49:48 +0700

    binh

--- src/pages/PaymentHistory.tsx
+++ src/pages/PaymentHistory.tsx
@@ -96,13 +96,13 @@ const PaymentHistory = () => {
     }
   })
   const [dataRecharges, setRecharges] = useState<any>([])
-  console.log(dataRecharges)
   useQuery({
     queryKey: ['admin-all-history'],
     queryFn: () => {
       return getWithrowRecharges({})
     },
     onSuccess: (data) => {
+      console.log(data.data);
       setRecharges(data.data)
     }
   })
@@ -156,7 +156,7 @@ const PaymentHistory = () => {
                 value={search}
                 onChange={(e) => setSearch(e.target.value)}
                 className='block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
-                placeholder='Search...'
+                placeholder='Nhập USERID'
               />
               <button
                 type='submit'
